a625403f62bf3cd7ad2c534674435d38a787b0ec
looklsh <looklsh38@gmail.com>
2024-04-02 18:00:43 +0900

    3-118 2024040201

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error(params) {
+  return (
+    <main className="error">
+      <h1>An Error occured!</h1>
+      <p>Failed to fetch meal date. Please try again later.</p>
+    </main>
+  );
+}
