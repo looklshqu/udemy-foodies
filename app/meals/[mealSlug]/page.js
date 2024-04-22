ea2e0ce23ee7871815cbc2d72e936d899f2e2f7c
looklsh <looklsh38@gmail.com>
2024-04-22 17:18:03 +0900

    3-130 2024042201

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -13,7 +13,11 @@ export default function MealDetailsPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image
+            src={`https://looklsh-nextjs-demo-users-image.s3.amazonaws.com/${meal.image}`}
+            alt={meal.title}
+            fill
+          />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
